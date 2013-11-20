8a03d66935f3cafeb6ac3aef348f3d52a1d8fcf0
robertocarroll <robertocarroll@gmail.com>
2013-11-20 21:52:00 +0000

    Updated the mapbox reference with the new map.

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ $.getJSON('ippf.geojson', function(data) {
 
      
 // Load the map 
-var map = L.mapbox.map('map', 'ippf.ippf', {
+var map = L.mapbox.map('map', 'ippf.gb9gdclc', {
 
     center: [25, -15],
     zoom: 2,
